0df2467f1ee6647ef1fc9a83b95fd3faab0bb591
ealborn <emma.alborn@gmail.com>
2018-01-14 12:25:01 +0100

    update

--- app/auth/authregister.php
+++ app/auth/authregister.php
@@ -7,17 +7,17 @@ require __DIR__.'/../autoload.php';
 if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
 
     $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $password = password_hash(filter_var($_POST['password'], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT);
 
-    $registerUser = $pdo->prepare("INSERT INTO user(Username, Email, Password) VALUES (:username, :email, :password)");
+    $registerUser = $pdo->prepare("INSERT INTO User (username, email, password) VALUES (:username, :email, :password)");
         if (!$registerUser) {
             die(var_dump($pdo->errorInfo()));
         }
     $registerUser->bindParam(':username', $username, PDO::PARAM_STR);
     $registerUser->bindParam(':email', $email, PDO::PARAM_STR);
-    $registerUser->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+    $registerUser->bindParam(':password', $password, PDO::PARAM_STR);
     $registerUser->execute();
-    
+
     redirect('../../login.php');
 }
