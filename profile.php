3802eee11c00b50b093ef5c759d02415a0cc4691
ealborn <emma.alborn@gmail.com>
2018-01-16 14:03:58 +0100

    add session check and form to upload

--- profile.php
+++ profile.php
@@ -1,8 +1,22 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php require __DIR__.'/views/header.php';
+
+//if a user is logged in
+$userSession = $_SESSION['userSession'] ?? false;
+$message = $_SESSION['message'] ?? '';
+unset($_SESSION['message']);
+?>
 
 <article>
     <h1>Profile</h1>
     <p>This is the new profile page.</p>
 </article>
+
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+      <label for="avatar">Choose an image to upload</label>
+      <input type="file" name="avatar" accept=".png" required>
+
+      <button type="submit">Upload</button>
+</form>
 
 <?php require __DIR__.'/views/footer.php'; ?>
