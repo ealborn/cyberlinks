d9413c71cbe59bd87abd00485752adfdd68148b3
ealborn <emma.alborn@gmail.com>
2018-01-16 13:47:13 +0100

    session rename

--- app/auth/authlogout.php
+++ app/auth/authlogout.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// // Remove the user session variable and redirect the user back to the homepage.
-// unset($_SESSION['username']);
-//
-// redirect('/');
+// Remove the user session variable and redirect the user back to the homepage.
 
-if (isset($_SESSION['authenticated'])) {
-    unset($_SESSION['authenticated']);
+if (isset($_SESSION['userSession'])) {
+    unset($_SESSION['userSession']);
     $_SESSION['message'] = 'You are successfully logged out!';
 }
 redirect('../../login.php');
