491e0fa73edd322777a1bb0a2cb26cc658ea3860
ealborn <emma.alborn@gmail.com>
2018-02-02 16:25:23 +0100

    profile styling

--- profile.php
+++ profile.php
@@ -28,7 +28,7 @@ unset($_SESSION['message']);
       <form action="upload.php" method="post" enctype="multipart/form-data">
         <label for="avatar">Update user image (only .png accepted)</label>
         <input type="file" name="avatar" accept=".png" required>
-        <button type="submit" class="btn btn-sm mt-1">Upload new avatar</button>
+        <button type="submit" class="btn btn-primary btn-sm">Upload new avatar</button>
       </form>
     </div>
   </div>
@@ -39,10 +39,7 @@ unset($_SESSION['message']);
   <p class="col-6"><?php echo $userSession['bio']; ?></p>
 </div>
 
-
-
-
-<!-- update user profile stuff-working!!!-->
+<!-- update user profile -->
 <?php
 // fetch from database again
 $statement = $pdo->prepare('SELECT * FROM User');
@@ -54,28 +51,25 @@ foreach ($results as $result) {
   $bio = $result['bio'];
   $email = $result['email'];
   $password = $result['password'];
-  //echo "$bio";
-  //echo "$email";
-  //echo "$password";
 } ?>
 
- <!-- working, do not touch!!! updates both to database and to page! -->
+ <!-- update biography, email or password form -->
     <form action="/app/auth/updateprofile.php" method="POST">
         <label for="textarea">Update biography</label>
         <textarea class="form-control" name="bio" rows="3"><?php echo $bio; ?></textarea>
-        <button type="submit" class="btn btn-sm mt-1">Update bio</button>
+        <button type="submit" class="btn btn-primary btn-sm">Update bio</button>
     </form>
 
     <form action="/app/auth/updateprofile.php" method="POST">
         <label for="input">Update e-mail</label>
         <input class="form-control" name="email" placeholder="<?php echo $email; ?>"></input>
-        <button type="submit" class="btn btn-sm mt-1">Update e-mail</button>
+        <button type="submit" class="btn btn-primary btn-sm">Update e-mail</button>
     </form>
 
     <form action="/app/auth/updateprofile.php" method="POST">
         <label for="input">Update password</label>
         <input class="form-control" name="password" type="password"></input>
-        <button type="submit" class="btn btn-sm mt-1">Update password</button>
+        <button type="submit" class="btn btn-primary btn-sm">Update password</button>
     </form>
 
 <?php require __DIR__.'/views/footer.php'; ?>
