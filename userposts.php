21136cfc6e8c53f22b04e3d5a8ec5b11d74fe119
ealborn <emma.alborn@gmail.com>
2018-02-02 14:55:00 +0100

    working perfectly, cleanup

--- userposts.php
+++ userposts.php
@@ -3,9 +3,8 @@ require __DIR__.'/views/header.php';
 $message = $_SESSION['message'] ?? '';
 unset($_SESSION['message']);
 $userSession = $_SESSION['userSession'] ?? false;
-// prepare database request to fetch users entries(posts)
+// prepare database request to fetch users entries(posts) for the logged in user
 $userPosts = $pdo->prepare("SELECT * from Entry JOIN Votes ON Votes.vote_id=Entry.entry_id WHERE Entry.poster=:userSession");
-//$userPosts = $pdo->prepare('SELECT Entry.*, User.user_id, User.username, User.avatar, Votes.* FROM Entry JOIN User ON Entry.poster=User.username JOIN Votes ON Votes.vote_id=Entry.entry_id');
 $userPosts->bindParam(':userSession', $userSession['username'], PDO::PARAM_STR);
 $userPosts->execute();
 $links = $userPosts->fetchAll(PDO::FETCH_ASSOC);
